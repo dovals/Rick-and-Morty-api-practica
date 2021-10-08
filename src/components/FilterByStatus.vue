85fadd2488bfd3e479a6aa817594f44fb72f35cf
dovals <eldono490@gmail.com>
2021-10-07 19:20:18 -0500

    Dovals

---
+++ src/components/FilterByStatus.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="filter">
+      <div class="item" @click="filter('')">
+          Todos
+      </div>
+      <div class="item" @click="filter('Alive')">
+          Vivo
+      </div>
+      <div class="item" @click="filter('Dead')">
+          Muerto
+      </div>
+      <div class="item" @click="filter('unknow')">
+          Desconocido
+      </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+    setup(){
+        const store = useStore()
+
+        const filter = ((status)=>{
+             store.dispatch('filterByStatus', status)
+        }) 
+        return {
+            filter
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.filter {
+        width: 400px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        border-radius: 10px;
+        overflow: hidden;
+    
+.item 
+    {
+        padding: 1rem 0.5rem;
+        background-color: var(--background-card);
+        text-align: center;
+        cursor: pointer;
+    
+&:hover{
+    color: var(--text-orange);
+        }
+    }
+}
+</style>
